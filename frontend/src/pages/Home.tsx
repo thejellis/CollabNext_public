57a705bcf41f78bd29465b7e15b8edfe10640f53
ikaay1 <imgbemele@gmail.com>
2024-06-25 09:55:50 -0500

    validate home form state

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,35 @@
+import { Field, Form, Formik } from 'formik';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
 
-import {Box, Button, Flex, Input, SimpleGrid, Text} from '@chakra-ui/react';
+import {
+	Box,
+	Button,
+	Flex,
+	FormControl,
+	FormErrorMessage,
+	Input,
+	SimpleGrid,
+	Text,
+} from '@chakra-ui/react';
+
+const validateSchema = Yup.object().shape({
+  institution: Yup.string().required('This field is required'),
+  type: Yup.string().notRequired(),
+  topic: Yup.string().notRequired(),
+  researcher: Yup.string().notRequired(),
+});
+
+const initialValues = {
+  institution: '',
+  type: '',
+  topic: '',
+  researcher: '',
+};
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <Box w={{lg: '700px'}} mx='auto' mt='1.5rem'>
       <Text
@@ -20,69 +47,120 @@ const Home = () => {
         py={{base: '1.5rem', lg: '2rem'}}
         mt='1rem'
       >
-        <SimpleGrid columns={{base: 1, lg: 2}} spacing={{base: 7, lg: '90px'}}>
-          {[{text: 'Organization'}, {text: 'Institution Type'}].map(
-            ({text}) => (
-              <Box key={text}>
-                <Input
-                  variant={'flushed'}
-                  focusBorderColor='white'
-                  borderBottomWidth={'2px'}
-                  color='white'
-                  fontSize={{lg: '20px'}}
-                  textAlign={'center'}
-                />
-                <Text
-                  fontSize={{base: '12px', lg: '15px'}}
-                  color='#FFFFFF'
-                  textAlign={'center'}
-                  mt='.7rem'
-                >
-                  {text}
-                </Text>
-              </Box>
-            ),
-          )}
-        </SimpleGrid>
-        <SimpleGrid
-          mt={{base: '1.35rem', lg: '1rem'}}
-          columns={{base: 1, lg: 2}}
-          spacing={{base: 7, lg: '90px'}}
+        <Formik
+          initialValues={initialValues}
+          enableReinitialize
+          validationSchema={validateSchema}
+          onSubmit={async ({institution, type, topic, researcher}) => {
+            console.log(`institution: ${institution}`);
+            console.log(`type: ${type}`);
+            console.log(`topic: ${topic}`);
+            console.log(`researcher: ${researcher}`);
+            navigate(
+              `?institution=${institution}&type=${type}&topic=${topic}&researcher=${researcher}`,
+            );
+          }}
         >
-          {[{text: 'Topic(s)'}, {text: 'Researcher Name'}].map(({text}) => (
-            <Box key={text}>
-              <Input
-                variant={'flushed'}
-                focusBorderColor='white'
-                borderBottomWidth={'2px'}
-                color='white'
-                fontSize={{lg: '20px'}}
-                textAlign={'center'}
-              />
-              <Text
-                fontSize={{base: '12px', lg: '15px'}}
-                color='#FFFFFF'
-                textAlign={'center'}
-                mt='.7rem'
+          {(props) => (
+            <Form>
+              <SimpleGrid
+                columns={{base: 1, lg: 2}}
+                spacing={{base: 7, lg: '90px'}}
               >
-                {text}
-              </Text>
-            </Box>
-          ))}
-        </SimpleGrid>
-        <Flex justifyContent={'flex-end'} mt={'3rem'}>
-          <Button
-            width={{base: '165px', lg: '205px'}}
-            height='41px'
-            background='#000000'
-            borderRadius={{base: '4px', lg: '6px'}}
-            fontSize={{base: '13px', lg: '18px'}}
-            color='#FFFFFF'
-            fontWeight={'500'}
-          >
-            Search
-          </Button>
-        </Flex>
+                {[
+                  {text: 'Organization', key: 'institution'},
+                  {text: 'Institution Type', key: 'type'},
+                ].map(({text, key}) => (
+                  <Box key={text}>
+                    <Field name={key}>
+                      {({field, form}: any) => (
+                        <FormControl
+                          isInvalid={form.errors[key] && form.touched[key]}
+                        >
+                          <Input
+                            variant={'flushed'}
+                            focusBorderColor='white'
+                            borderBottomWidth={'2px'}
+                            color='white'
+                            fontSize={{lg: '20px'}}
+                            textAlign={'center'}
+                            {...field}
+                          />
+                          <FormErrorMessage>
+                            {form.errors[key]}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Text
+                      fontSize={{base: '12px', lg: '15px'}}
+                      color='#FFFFFF'
+                      textAlign={'center'}
+                      mt='.7rem'
+                    >
+                      {text}
+                    </Text>
+                  </Box>
+                ))}
+              </SimpleGrid>
+              <SimpleGrid
+                mt={{base: '1.35rem', lg: '1rem'}}
+                columns={{base: 1, lg: 2}}
+                spacing={{base: 7, lg: '90px'}}
+              >
+                {[
+                  {text: 'Topic(s)', key: 'topic'},
+                  {text: 'Researcher Name', key: 'researcher'},
+                ].map(({text, key}) => (
+                  <Box key={text}>
+                    <Field name={key}>
+                      {({field, form}: any) => (
+                        <FormControl
+                          isInvalid={form.errors[key] && form.touched[key]}
+                        >
+                          <Input
+                            variant={'flushed'}
+                            focusBorderColor='white'
+                            borderBottomWidth={'2px'}
+                            color='white'
+                            fontSize={{lg: '20px'}}
+                            textAlign={'center'}
+                            {...field}
+                          />
+                          <FormErrorMessage>
+                            {form.errors[key]}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Text
+                      fontSize={{base: '12px', lg: '15px'}}
+                      color='#FFFFFF'
+                      textAlign={'center'}
+                      mt='.7rem'
+                    >
+                      {text}
+                    </Text>
+                  </Box>
+                ))}
+              </SimpleGrid>
+              <Flex justifyContent={'flex-end'} mt={'3rem'}>
+                <Button
+                  width={{base: '165px', lg: '205px'}}
+                  height='41px'
+                  background='#000000'
+                  borderRadius={{base: '4px', lg: '6px'}}
+                  fontSize={{base: '13px', lg: '18px'}}
+                  color='#FFFFFF'
+                  fontWeight={'500'}
+                  type='submit'
+                >
+                  Search
+                </Button>
+              </Flex>
+            </Form>
+          )}
+        </Formik>
       </Box>
     </Box>
   );
