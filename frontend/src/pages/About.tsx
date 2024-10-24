a7e137e7a6c5e7fb658d16dc613cc09608a59b97
Lew Lefton <lew.lefton@gatech.edu>
2024-10-23 23:05:44 -0400

    Cleanup About Us typos

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -51,8 +51,15 @@ const About = () => {
               style={{ color: 'cornsilk', textDecoration: 'underline' }}
             >
               Georgia Tech
-            </a>{', '}and the Atlanta
-            University Center, and is now being developed jointly by{' '}
+            </a>{', '}and the{' '}
+            <a
+              href='https://aucenter.edu/'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            >
+              Atlanta University Center
+            </a>, and is now being developed jointly by{' '}
             <a
               href='https://www.fisk.edu/'
               target='_blank'
@@ -84,7 +91,7 @@ const About = () => {
               style={{ color: 'cornsilk', textDecoration: 'underline' }}
             >
               Texas Southern University
-            </a>{', '}and{ }
+            </a>{', '}and{' '}
             <a
               href='https://buffalo.edu/'
               target='_blank'
@@ -127,7 +134,7 @@ const About = () => {
             fontSize={{base: '12px', lg: '16px'}}
           >
             Our goal is to <b>develop a knowledge graph based on people,
-              organizations, and research topics</b>. We are adopting an intentional
+            organizations, and research topics</b>. We are adopting an intentional
             design approach, <b>initially prioritizing HBCUs and emerging
             researchers</b> in a deliberate effort to counterbalance the{' '}
             <a
@@ -138,7 +145,7 @@ const About = () => {
             >
               Matthew effect
             </a>
-            , a naturally accumulated advantage of well - resourced research
+            , a naturally accumulated advantage of well-resourced research
             organizations.
           </Text>
           <Text
@@ -158,9 +165,17 @@ const About = () => {
             color='#FFFFFF'
             fontSize={{ base: '12px', lg: '16px' }}
           >
-            We utilize open science data sources, follow
-            human - centered design principles, and leverage state - of - the -
-            art algorithms to build a platform that will help identify existing
+            We utilize open science{' '}
+            <a
+              href='https://collabnext.io/data'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            >
+             data sources
+            </a>, follow
+            human-centered design principles, and leverage state-of-the-art
+            algorithms to build a platform that will help identify existing
             and potentially new research partnerships.
           </Text>
         </Box>
@@ -176,26 +191,25 @@ const About = () => {
             mt='.45rem'
             lineHeight='24px'
             color='#FFFFFF'
-            fontSize={{base: '12px', lg: '16px'}}
+            fontSize={{ base: '12px', lg: '16px' }}
           >
-            Our project is being developed as part of the{' '}
+            <b>The current CollabNext toolis an alpha version</b>. Our{' '}
             <a
-              href='https://www.proto-okn.net'
+              href='https://collabnext.io/technology'
               target='_blank'
               rel='noreferrer'
               style={{ color: 'cornsilk', textDecoration: 'underline' }}
             >
-              Prototype Open Knowledge Network
-            </a>.
-          </Text>
-          <Text
-            mt='.45rem'
-            lineHeight='24px'
-            color='#FFFFFF'
-            fontSize={{ base: '12px', lg: '16px' }}
-          >
-            Our Technology and Data Sources are described on other pages.
-          <b>The current CollabNext toolis an alpha version</b>.
+              Technology
+            </a>{' '}and{' '}
+            <a
+              href='https://collabnext.io/data'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            >
+              data sources
+            </a> are described on other pages.
           We are developing future versions, moving from beta to
           production over the next two years. Our{' '}
           <a
@@ -204,7 +218,7 @@ const About = () => {
             rel='noreferrer'
             style={{ color: 'cornsilk', textDecoration: 'underline' }}
           >
-            proof - of - concept deliverable
+            proof-of-concept deliverable{' '}
           </a>
           which preceded the alpha version, is available for comparison.
           </Text>
@@ -214,9 +228,24 @@ const About = () => {
           color='#FFFFFF'
           fontSize={{ base: '12px', lg: '16px' }}
         >
-          The design and strategic direction of the project are
-          guided by our Advisory Group. This group consists of
-          selected members of the Leadership Teamas well other
+          The design and strategic direction of the project are guided by our{' '}
+            <a
+              href='https://collabnext.io/team'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            >
+              Advisory Group
+            </a>. This group consists of
+            selected members of the{' '}
+            <a
+              href='https://collabnext.io/team'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            >
+              Leadership Team
+            </a>{' '}as well as other
           individuals who have a unique and valuable perspective
           on our project (e.g. HBCU faculty, underrepresented groups in
           STEM, etc.). The group serves as a standing focus group and
@@ -228,7 +257,14 @@ const About = () => {
             color='#FFFFFF'
             fontSize={{ base: '12px', lg: '16px' }}
           >
-          We are also fortunate to have key strategic Partnerships
+            We are also fortunate to have key strategic{' '}
+            <a
+              href='https://collabnext.io/team'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            > Partnerships
+              </a>{' '}
           with academic, non-profit, and corporate organizations that
           serve as data providers, and resources with expertise. Partners
           meet with the Leadership Team quarterly to identify collaboration
@@ -249,7 +285,13 @@ const About = () => {
           after it is built and operational at the end of the performance
           period of the NSF grant. If you are interested in discussing
           this or if you know of others who may be interested, please
-          contact us.
+            <a
+              href='https://collabnext.io/contact'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            > contact us
+              </a>.
         </Text>
         </Box>
       </Box>
